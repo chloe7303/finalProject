7a69962f02b1c9dd1e96dad36ddbd94ba0985fcd
chloe7303 <57607232+chloe7303@users.noreply.github.com>
2021-02-01 13:50:07 +0800

    Update projects.js

--- controllers/projects.js
+++ controllers/projects.js
@@ -150,8 +150,8 @@ export const getImage = async (req, res) => {
       method: 'GET',
       url: 'http://' + process.env.FTP_HOST + '/' + process.env.FTP_USER + '/' + req.params.file,
       responseType: 'stream'
-    }).then(res => {
-      res.data.pipe(res)
+    }).then(ress => {
+      ress.data.pipe(res)
     }).catch(error => {
       console.log(error)
       res.status(error.response.status).send({ success: false, message: '取得圖片失敗' })
